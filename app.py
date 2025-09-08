6e9fdbfc7b3aa7b3e68a24a8a1992364b3c6d514
Piyush Pal <piyushpal2685@gmail.com>
2025-09-08 13:27:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Flask Deployment Successful on Render!"
+
+@app.route("/about")
+def about():
+    return "This is a small project deployed using Flask on Render."
+
+if __name__ == "__main__":
+    app.run(debug=True)
